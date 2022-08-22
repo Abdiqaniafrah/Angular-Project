02b1b2d1d96c9a737e41e32eccf81eda3796138c
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-08-22 16:50:58 +0100

    setup and typeScript introduction

--- project-template/src/app/app.module.ts
+++ project-template/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
